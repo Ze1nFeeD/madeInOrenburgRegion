119a450191159939eaf8756ed965d2a1253d0ed1
Ze1nFeeD <gura.dmitriy@internet.ru>
2023-10-08 21:02:19 +0500

    Image fix

---
+++ app/src/main/java/com/example/madeinorenburgregion/CultureActivity.kt
@@ -0,0 +1,11 @@
+package com.example.madeinorenburgregion
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CultureActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_culture)
+    }
+}
--- app/src/main/java/com/example/madeinorenburgregion/MainFragment.kt
+++ app/src/main/java/com/example/madeinorenburgregion/MainFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
 
 
 class MainFragment : Fragment() {
@@ -13,6 +14,11 @@ class MainFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        val view = inflater.inflate(R.layout.fragment_main, container, false)
+          val culture: ConstraintLayout = view.findViewById(R.id.cultureClick)
+        culture.setOnClickListener{
+            //активити
+        }
+        return view
     }
 }
