4c54b369c35dd7744e1cd301fb641b9e941e425e
Ze1nFeeD <gura.dmitriy@internet.ru>
2023-10-29 10:28:14 +0500

    edit color background and edit activity_attractions.xml

--- app/src/main/java/com/example/madeinorenburgregion/AttractionsActivity.kt
+++ app/src/main/java/com/example/madeinorenburgregion/AttractionsActivity.kt
@@ -9,9 +9,17 @@ class AttractionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_attractions)
-        val museimClick : ConstraintLayout = findViewById(R.id.museimClick)
+        val museimClick: ConstraintLayout = findViewById(R.id.museimClick)
         museimClick.setOnClickListener {
             startActivity(Intent(this, OrenMuseimActivity::class.java))
+        }
+        val parkClick: ConstraintLayout = findViewById(R.id.parkclick)
+        parkClick.setOnClickListener{
+            startActivity(Intent(this, ParkPobediOrenActivity::class.java))
+        }
+        val irilkClick: ConstraintLayout = findViewById(R.id.iriklinhranClick)
+        irilkClick.setOnClickListener {
+            startActivity(Intent(this, IriklinskoeActivity::class.java))
         }
     }
 }
