40bcaa64bfdab7cdfff44cbfdf17a48f03a4b079
Ze1nFeeD <gura.dmitriy@internet.ru>
2023-10-29 18:09:44 +0500

    add image park pobedi and iriklinskoe

--- app/src/main/java/com/example/madeinorenburgregion/IriklinskoeActivity.kt
+++ app/src/main/java/com/example/madeinorenburgregion/IriklinskoeActivity.kt
@@ -5,11 +5,33 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.viewpager.widget.ViewPager
+import com.tbuonomo.viewpagerdotsindicator.DotsIndicator
 
 class IriklinskoeActivity : AppCompatActivity() {
+    lateinit var viewPager: ViewPager
+    lateinit var viewPagerAdapter: ViewPagerAdapter
+    lateinit var imageList: List<Int>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_iriklinskoe)
+        viewPager = findViewById(R.id.idViewPager)
+
+        val dotsIndicator = findViewById<DotsIndicator>(R.id.dots_indicator)
+
+        imageList = ArrayList<Int>()
+        imageList = imageList + R.drawable.iriklin1
+        imageList = imageList + R.drawable.iriklin2
+        imageList = imageList + R.drawable.iriklin3
+        imageList = imageList + R.drawable.iriklin4
+        imageList = imageList + R.drawable.iriklin5
+
+
+        viewPagerAdapter = ViewPagerAdapter(this@IriklinskoeActivity, imageList)
+
+
+        viewPager.adapter = viewPagerAdapter
+        dotsIndicator.attachTo(viewPager)
     }
     fun mapsIriklinskoe(view: View) {
         val geoUriString = "geo:51.890927, 58.883228?z=15"
--- app/src/main/java/com/example/madeinorenburgregion/ParkPobediOrenActivity.kt
+++ app/src/main/java/com/example/madeinorenburgregion/ParkPobediOrenActivity.kt
@@ -5,12 +5,33 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.viewpager.widget.ViewPager
+import com.tbuonomo.viewpagerdotsindicator.DotsIndicator
 
 class ParkPobediOrenActivity : AppCompatActivity() {
+    lateinit var viewPager: ViewPager
+    lateinit var viewPagerAdapter: ViewPagerAdapter
+    lateinit var imageList: List<Int>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_park_pobedi_oren)
+        viewPager = findViewById(R.id.idViewPager)
 
+        val dotsIndicator = findViewById<DotsIndicator>(R.id.dots_indicator)
+
+        imageList = ArrayList<Int>()
+        imageList = imageList + R.drawable.park4
+        imageList = imageList + R.drawable.park5
+        imageList = imageList + R.drawable.park6
+        imageList = imageList + R.drawable.park7
+        imageList = imageList + R.drawable.park8
+
+
+        viewPagerAdapter = ViewPagerAdapter(this@ParkPobediOrenActivity, imageList)
+
+
+        viewPager.adapter = viewPagerAdapter
+        dotsIndicator.attachTo(viewPager)
     }
     fun maps(view: View) {
         val geoUriString = "geo:51.776910, 55.165685?z=15"
