5bafcce53e28d5beabdb879a85ed97a098f7de89
Ze1nFeeD <gura.dmitriy@internet.ru>
2023-10-29 16:46:34 +0500

    add image in OrenMuseim

--- app/src/main/java/com/example/madeinorenburgregion/OrenMuseimActivity.kt
+++ app/src/main/java/com/example/madeinorenburgregion/OrenMuseimActivity.kt
@@ -6,13 +6,32 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageSwitcher
+import androidx.viewpager.widget.ViewPager
+import com.tbuonomo.viewpagerdotsindicator.DotsIndicator
 
 class OrenMuseimActivity : AppCompatActivity() {
-
+    lateinit var viewPager: ViewPager
+    lateinit var viewPagerAdapter: ViewPagerAdapter
+    lateinit var imageList: List<Int>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_oren_museim)
+        viewPager = findViewById(R.id.idViewPager)
+        val dotsIndicator = findViewById<DotsIndicator>(R.id.dots_indicator)
 
+        imageList = ArrayList<Int>()
+        imageList = imageList + R.drawable.orenmuseim1
+        imageList = imageList + R.drawable.orenmuseim2
+        imageList = imageList + R.drawable.orenmuseim3
+        imageList = imageList + R.drawable.orenmuseim4
+        imageList = imageList + R.drawable.orenmuseim5
+
+
+        viewPagerAdapter = ViewPagerAdapter(this@OrenMuseimActivity, imageList)
+
+
+        viewPager.adapter = viewPagerAdapter
+        dotsIndicator.attachTo(viewPager)
     }
 
     fun maps(view: View) {
