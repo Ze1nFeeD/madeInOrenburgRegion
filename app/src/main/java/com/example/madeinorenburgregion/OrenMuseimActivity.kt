badf168015eb54cc473737da369041df3d81ebd6
Ze1nFeeD <gura.dmitriy@internet.ru>
2023-10-29 15:38:47 +0500

    save commit

--- app/src/main/java/com/example/madeinorenburgregion/OrenMuseimActivity.kt
+++ app/src/main/java/com/example/madeinorenburgregion/OrenMuseimActivity.kt
@@ -5,16 +5,24 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageSwitcher
 
 class OrenMuseimActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_oren_museim)
+
     }
+
     fun maps(view: View) {
         val geoUriString = "geo:51.755481, 55.108387?z=15"
         val geoUri: Uri = Uri.parse(geoUriString)
         val mapIntent = Intent(Intent.ACTION_VIEW, geoUri)
         startActivity(mapIntent)
     }
+
+
+
+
 }
